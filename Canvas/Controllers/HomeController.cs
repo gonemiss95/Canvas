029e323d83c1bb9fd09564cd821700a5624e2326
karlun <karlun@DESKTOP-1IGUG7O>
2023-09-07 15:02:39 +0800

    Added canvas & basic filters

--- Canvas/Controllers/HomeController.cs
+++ Canvas/Controllers/HomeController.cs
@@ -1,22 +1,38 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using System.Drawing;
 
 namespace Canvas.Controllers
 {
+    [RoutePrefix("Home")]
     public class HomeController : Controller
     {
+        [Route("Index")]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Route("Scan")]
         public ActionResult Scan()
         {
             return View();
         }
 
+        [Route("Edit")]
         public ActionResult Edit()
         {
             return View();
+        }
+
+        [Route("GetImage")]
+        public JsonResult GetImage()
+        {
+            Image img = Image.FromFile("D:\\Work Document\\a.jpg");
+            ImageConverter imgConverter = new ImageConverter();
+            byte[] imgBytes = imgConverter.ConvertTo(img, typeof(byte[])) as byte[];
+            string imgStr = $"data:image/gif;base64,{Convert.ToBase64String(imgBytes)}";
+            return Json(imgStr, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Canvas/Scripts/Canvas.js
@@ -0,0 +1,78 @@
+﻿var inputBrightness = document.getElementById("inBrightness");
+var inputSaturation = document.getElementById("inSaturation");
+var inputContrast = document.getElementById("inContrast");
+var inputGrayscale = document.getElementById("inGrayscale");
+var inputInversion = document.getElementById("inInversion");
+
+let image = new Image();
+let canvas = document.getElementById("canvas");
+let canvasCtx= canvas.getContext("2d");
+
+
+$(document).ready(function () {
+    getImage();
+    $("#lblFilter").click();
+});
+
+function getImage() {
+    $.ajax({
+        cache: false,
+        type: "POST",
+        url: GetImageURL,
+        success: function (data) {
+            image.src = data;
+
+            image.addEventListener("load", function () {
+                resetFilter();
+            });
+        }
+    });
+}
+
+
+//Tab functions
+function filterClick() {
+    $('.divTabDetailFilter').css("display", "block");
+
+    $('#lblFilter').css({ "font-size": "18px", "text-decoration": "underline", "text-underline-offset": "8px" });
+    $('#lblEditor').css({ "font-size": "16px", "text-decoration": "none" });
+}
+
+function editorClick() {
+    $('.divTabDetailFilter').css("display", "none");
+
+    $('#lblFilter').css({ "font-size": "16px", "text-decoration": "none" });
+    $('#lblEditor').css({ "font-size": "18px", "text-decoration": "underline", "text-underline-offset": "8px" });
+}
+
+
+//Filter functions
+function sliderValueChanged(filterKey) {
+    const filter = window[`input${filterKey}`];
+    $(`#lbl${filterKey}Value`).text(`${filter.value}%`);
+    applyFilter();
+}
+
+function resetFilter() {
+    canvas.width = image.width;
+    canvas.height = image.height;
+
+    inputBrightness.value = "100";
+    inputSaturation.value = "100";
+    inputContrast.value = "100";
+    inputGrayscale.value = "0";
+    inputInversion.value = "0";
+
+    $("#lblBrightnessValue").text(`${inputBrightness.value}%`);
+    $("#lblSaturationValue").text(`${inputSaturation.value}%`);
+    $("#lblContrastValue").text(`${inputContrast.value}%`);
+    $("#lblGrayscaleValue").text(`${inputGrayscale.value}%`);
+    $("#lblInversionValue").text(`${inputInversion.value}%`);
+
+    applyFilter();
+}
+
+function applyFilter() {
+    canvasCtx.filter = `brightness(${inputBrightness.value}%) saturate(${inputSaturation.value}%) contrast(${inputContrast.value}%) grayscale(${inputGrayscale.value}%) invert(${inputInversion.value}%)`;
+    canvasCtx.drawImage(image, 0, 0);
+};
